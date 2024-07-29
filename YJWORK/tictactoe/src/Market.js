3532009c3e7020bf56f5ae950711f33933ed7cd7
= <=>
2024-07-29 22:13:41 +0900

    집에서 후,,,

--- YJWORK/tictactoe/src/Market.js
+++ YJWORK/tictactoe/src/Market.js
@@ -11,7 +11,7 @@ const PRODUCTS = [
     {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
     {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
     {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
-];
+  ];
 
 
 //초록색
