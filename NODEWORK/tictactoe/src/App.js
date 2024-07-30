980d2ecd68cc4a8bc95c2c9b4c7c2e3469b6bb49
= <=>
2024-07-30 17:49:35 +0900

    dd

--- NODEWORK/tictactoe/src/App.js
+++ NODEWORK/tictactoe/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import Market from './Market';
+import { useState } from 'react';
 
 
-function Square( {value} ) {
+function Square() {
+
+  const [value, setValue] = useState(null);
 
   function handleClick() {
     console.log('clicked!');
@@ -17,19 +20,19 @@ function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
